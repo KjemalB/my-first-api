589fcd57d0a5eae0e5c6e6eba6110dc98d549904
Khalil Brewington <kbrewing02@gmail.com>
2022-07-11 14:16:09 -0400

    updates

---
+++ src/main/java/Brewington/Wilson/myfirstapi/MyFirstApiApplication.java
@@ -0,0 +1,13 @@
+package Brewington.Wilson.myfirstapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstApiApplication.class, args);
+	}
+
+}
